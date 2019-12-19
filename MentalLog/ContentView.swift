6d577d2821e320dc6c5cba368b42b3e2c3ecc212
Cameron Bardell <cambardell3@gmail.com>
2019-12-19 00:55:19 -0500

    Initial Commit

---
+++ MentalLog/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MentalLog
+//
+//  Created by Cameron Bardell on 2019-12-19.
+//  Copyright © 2019 Cameron Bardell. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
